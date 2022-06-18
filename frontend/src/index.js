46490aa942ef6cb943e7b3cb00fc877904656289
Bishwajit <bishwajit0020@gmail.com>
2022-06-18 17:03:28 +0600

    Node module fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,33 +7,33 @@ import data from './mock-data.json';
 import axios from './axios';
 
 const userFields = [
-	'firstName',
-	'lastName',
-	'email',
-	'mobileNo'
+  'firstName',
+  'lastName',
+  'email',
+  'mobileNo'
 ];
 
 const getClearState = () => {
-	const states = {};
-	userFields.forEach(field => states[field] = "");
-	return states;
+  const states = {};
+  userFields.forEach(field => states[field] = "");
+  return states;
 }
 class UserTable extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			editedUser: null,
-			selectedEditUserId: null
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      editedUser: null,
+      selectedEditUserId: null
+    };
+  }
 
-	handleEditUserSelection = (user) => {
-		this.setState({
-			selectedEditUserId: user._id,
-			editedUser: {...user}
-		})
-	}
+  handleEditUserSelection = (user) => {
+    this.setState({
+      selectedEditUserId: user._id,
+      editedUser: { ...user }
+    })
+  }
 
   handleUserSelectionCancel = () => {
     this.setState({
@@ -42,248 +42,248 @@ class UserTable extends React.Component {
     })
   }
 
-	handleUserFieldEdit = (event) => {
-		const fieldName = event.target.getAttribute("name");
-		const fieldValue = event.target.value;
+  handleUserFieldEdit = (event) => {
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
 
-		console.log(`selected field ${fieldName}, val: ${fieldValue}`)
+    console.log(`selected field ${fieldName}, val: ${fieldValue}`)
 
-		this.setState((prevState) => ({
+    this.setState((prevState) => ({
       // [fieldName] : fieldValue
-      editedUser: { 
-        ...prevState.editedUser, 
+      editedUser: {
+        ...prevState.editedUser,
         [fieldName]: fieldValue
-			}
-		}));
-	}
+      }
+    }));
+  }
 
 
-	render () {
-			const columns = ['First Name', 'Fast Name', 'Email', 'Mobile No', 'Actions'];
-			const { users, onSaveEditedUser, onDeleteUserClick} = this.props;
+  render() {
+    const columns = ['First Name', 'Fast Name', 'Email', 'Mobile No', 'Actions'];
+    const { users, onSaveEditedUser, onDeleteUserClick } = this.props;
 
 
-			const userRows = users.map((user, ind) => {
-				// console.log(`userid: ${user._id}, selected: ${selectedEditUserId}`)
-				return (
-          <tr key={user._id}>
-            {(user._id === this.state.selectedEditUserId) ? (
+    const userRows = users.map((user, ind) => {
+      // console.log(`userid: ${user._id}, selected: ${selectedEditUserId}`)
+      return (
+        <tr key={user._id}>
+          {(user._id === this.state.selectedEditUserId) ? (
 
-              <EditableUserRow
-                editedIndex={ind}
-                user={this.state.editedUser}
-                onUserInfoEdit={this.handleUserFieldEdit}
-                onSaveEditedUser={(e, ind, user) => {
-                  this.setState({
-                    selectedEditUserId: null
-                  })
-                  onSaveEditedUser(e, ind, user)
-								}}
-                onUserSelectionCancel={this.handleUserSelectionCancel}
-              />
-            ) : (
-              <ReadOnlyUserRow
-                user={user}
-                onEditUserSelection={this.handleEditUserSelection}
-                onDeleteUserClick={onDeleteUserClick}/>
-            )}
-          </tr>
-        );
-			});
+            <EditableUserRow
+              editedIndex={ind}
+              user={this.state.editedUser}
+              onUserInfoEdit={this.handleUserFieldEdit}
+              onSaveEditedUser={(e, ind, user) => {
+                this.setState({
+                  selectedEditUserId: null
+                })
+                onSaveEditedUser(e, ind, user)
+              }}
+              onUserSelectionCancel={this.handleUserSelectionCancel}
+            />
+          ) : (
+            <ReadOnlyUserRow
+              user={user}
+              onEditUserSelection={this.handleEditUserSelection}
+              onDeleteUserClick={onDeleteUserClick} />
+          )}
+        </tr>
+      );
+    });
 
-			return (
-					<div>
-						<form>
-							<table>
-								<UserTableHeader columns={columns}/>
-								<tbody> 
-									{userRows}	
-								</tbody>
-							</table>
-						</form>
-					</div>
-			);
-	}
+    return (
+      <div>
+        <form>
+          <table>
+            <UserTableHeader columns={columns} />
+            <tbody>
+              {userRows}
+            </tbody>
+          </table>
+        </form>
+      </div>
+    );
+  }
 }
 
 class EditableUserRow extends React.Component {
 
 
-	render() {		
-		const {editedIndex, onUserInfoEdit, onSaveEditedUser, onUserSelectionCancel} = this.props;
-		const fields = userFields;
-		const user = {...this.props.user};
+  render() {
+    const { editedIndex, onUserInfoEdit, onSaveEditedUser, onUserSelectionCancel } = this.props;
+    const fields = userFields;
+    const user = { ...this.props.user };
 
-		return (
-			<>
-				{	fields.map((field) => {
-						return (
-							<td key={field}> 
-								<input 
-									type="text"
-									name={field}
-									value={user? user[field]: ""}
-									required="required"
-									onChange={onUserInfoEdit}
-								/>
-							</td>
-						);
-					})
-				}
-				<td>
-					<button onClick={(e) => onSaveEditedUser(e, editedIndex, user)}> Save </button>
-					<button onClick={onUserSelectionCancel}> Cancel  </button>
-				</td>
-			</>
+    return (
+      <>
+        {fields.map((field) => {
+          return (
+            <td key={field}>
+              <input
+                type="text"
+                name={field}
+                value={user ? user[field] : ""}
+                required="required"
+                onChange={onUserInfoEdit}
+              />
+            </td>
+          );
+        })
+        }
+        <td>
+          <button onClick={(e) => onSaveEditedUser(e, editedIndex, user)}> Save </button>
+          <button onClick={onUserSelectionCancel}> Cancel  </button>
+        </td>
+      </>
 
-		)
-	}
+    )
+  }
 }
 
 class ReadOnlyUserRow extends React.Component {
 
-	render() {
-		const {user, onEditUserSelection, onDeleteUserClick} = this.props;
-		const fields = userFields;
+  render() {
+    const { user, onEditUserSelection, onDeleteUserClick } = this.props;
+    const fields = userFields;
 
 
-		return (
-			<>
-				{	fields.map((field) => {
-						return (
-							<td key={field}> {user[field] || ""} </td>
-						);
-					})
-				}
-				<td> 
-					<button
-						type='submit'
-						onClick={(e) => onEditUserSelection(user)}
-					> Edit
-					</button>
-
-					<button 
-            type='submit' 
-            onClick={(e) => onDeleteUserClick(e, user)}
-            > 
-            Delete 
+    return (
+      <>
+        {fields.map((field) => {
+          return (
+            <td key={field}> {user[field] || ""} </td>
+          );
+        })
+        }
+        <td>
+          <button
+            type='submit'
+            onClick={(e) => onEditUserSelection(user)}
+          > Edit
           </button>
-				</td>
 
-			</>
-		);
-	}	
+          <button
+            type='submit'
+            onClick={(e) => onDeleteUserClick(e, user)}
+          >
+            Delete
+          </button>
+        </td>
+
+      </>
+    );
+  }
 }
 
 class UserTableHeader extends React.Component {
-	render () {
-			const {columns} = this.props;
-			const headerColumns = columns.map( (column) => {
-					return (
-							<th key={column}> {column} </th>
-					)
-			});
-			return (
-				<thead> 
-					<tr>
-						{headerColumns}
-					</tr>
-				</thead>
-			);
-	}
+  render() {
+    const { columns } = this.props;
+    const headerColumns = columns.map((column) => {
+      return (
+        <th key={column}> {column} </th>
+      )
+    });
+    return (
+      <thead>
+        <tr>
+          {headerColumns}
+        </tr>
+      </thead>
+    );
+  }
 }
 
 class CreateUserForm extends React.Component {
-	constructor(props) {
-		super(props);
-		
-		const clearState = getClearState();
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			...clearState
-		};
-	}
+    const clearState = getClearState();
 
-	handleFormChange = (event) => {
-		const fieldName = event.target.getAttribute("name");
-		const fieldValue = event.target.value;
+    this.state = {
+      ...clearState
+    };
+  }
 
-		this.setState({
-			[fieldName] : fieldValue
-		});
-	}
+  handleFormChange = (event) => {
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
 
-	handleSubmit = (e) => {
-		e.preventDefault();
-		console.log(this.state);
-		const newUser = {...this.state};
+    this.setState({
+      [fieldName]: fieldValue
+    });
+  }
 
-		this.setState(getClearState);
-		this.props.onUserCreation(newUser);
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    const newUser = { ...this.state };
 
-		// alert("Form submitted!")
-	} 
+    this.setState(getClearState);
+    this.props.onUserCreation(newUser);
 
-	render() {		
-		return (
-			<div>
-				<h2> Add a user </h2>
-				<form onSubmit={this.handleSubmit}>
-					<input 
-						type="text"
-						name='firstName' 
-						value={this.state.firstName}
-						required="required"
-						placeholder='Enter first name...'
-						onChange={this.handleFormChange}
-					/>
-					<input 
-						type="text"
-						name='lastName' 
-						value={this.state.lastName}
-						required="required"
-						placeholder='Enter last name...'
-						onChange={this.handleFormChange}
-					/>
-					<input 
-						type="text"
-						name='email' 
-						value={this.state.email}
-						required="required"
-						placeholder='Enter Email...'
-						onChange={this.handleFormChange}
-					/>
-					<input 
-						type="text"
-						name='mobileNo' 
-						value={this.state.mobileNo}
-						required="required"
-						placeholder='Enter mobile Num...'
-						onChange={this.handleFormChange}
-					/>
+    // alert("Form submitted!")
+  }
 
-					<button type='submit'>Add</button>
-					</form>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <h2> Add a user </h2>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name='firstName'
+            value={this.state.firstName}
+            required="required"
+            placeholder='Enter first name...'
+            onChange={this.handleFormChange}
+          />
+          <input
+            type="text"
+            name='lastName'
+            value={this.state.lastName}
+            required="required"
+            placeholder='Enter last name...'
+            onChange={this.handleFormChange}
+          />
+          <input
+            type="text"
+            name='email'
+            value={this.state.email}
+            required="required"
+            placeholder='Enter Email...'
+            onChange={this.handleFormChange}
+          />
+          <input
+            type="text"
+            name='mobileNo'
+            value={this.state.mobileNo}
+            required="required"
+            placeholder='Enter mobile Num...'
+            onChange={this.handleFormChange}
+          />
+
+          <button type='submit'>Add</button>
+        </form>
+      </div>
+    )
+  }
 }
-  
+
 class App extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			users: data,
-			shouldReload: false
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: data,
+      shouldReload: false
+    };
+  }
 
-	handleSaveEditedUser = (event, index, user) => {
-		event.preventDefault();
+  handleSaveEditedUser = (event, index, user) => {
+    event.preventDefault();
 
-		console.log(`Saved user ind ${index}`)
-		console.log(user)
+    console.log(`Saved user ind ${index}`)
+    console.log(user)
 
     axios
       .patch(`/edit/${user._id}`, user)
@@ -296,7 +296,7 @@ class App extends React.Component {
       .catch((e) => {
         console.log(e);
       })
-	}
+  }
 
   handleDeleteUser = (event, user) => {
     event.preventDefault();
@@ -315,7 +315,7 @@ class App extends React.Component {
 
   }
 
-	handleUserCreation = (user) => {
+  handleUserCreation = (user) => {
 
     console.log(`Attempting to create user`, user);
 
@@ -331,7 +331,7 @@ class App extends React.Component {
         console.log(e);
       })
 
-	}
+  }
 
   componentDidMount() {
     axios
@@ -368,27 +368,24 @@ class App extends React.Component {
         });
     }
   }
-	
-	render() {
-			return (
-					<div className="app-container">
-						<UserTable 
-							users={this.state.users}
-							onSaveEditedUser={this.handleSaveEditedUser}
-              onDeleteUserClick={this.handleDeleteUser}
-							// onEditUserSelection={this.handleEditUserSelection}
-							// selectedEditUserId={this.state.selectedEditUserId}
-							/>
-						<CreateUserForm 
-							onUserCreation={this.handleUserCreation}
-						/>
-					</div>
-			);
-	}
+
+  render() {
+    return (
+      <div className="app-container">
+        <UserTable
+          users={this.state.users}
+          onSaveEditedUser={this.handleSaveEditedUser}
+          onDeleteUserClick={this.handleDeleteUser}
+        />
+        <CreateUserForm
+          onUserCreation={this.handleUserCreation}
+        />
+      </div>
+    );
+  }
 }
 
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
-  
